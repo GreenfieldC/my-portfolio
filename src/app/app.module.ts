352647f88b64fbc5027483cc6a3f6006946a02b5
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-04-13 12:34:27 +0200

    generated my-work component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { HeaderComponent } from './header/header.component';
 import { GreetingComponent } from './greeting/greeting.component';
 import { MainSiteComponent } from './main-site/main-site.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyWorkComponent } from './my-work/my-work.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, GreetingComponent, MainSiteComponent, MySkillsComponent],
+  declarations: [AppComponent, HeaderComponent, GreetingComponent, MainSiteComponent, MySkillsComponent, MyWorkComponent],
   imports: [BrowserModule, NgbModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
