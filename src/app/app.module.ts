57c8cf39dbf211f031803131e767962fad007041
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-04-13 14:15:08 +0200

    material design implemented, btns  my-work section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,11 +11,21 @@ import { GreetingComponent } from './greeting/greeting.component';
 import { MainSiteComponent } from './main-site/main-site.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, GreetingComponent, MainSiteComponent, MySkillsComponent, MyWorkComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		GreetingComponent,
+		MainSiteComponent,
+		MySkillsComponent,
+		MyWorkComponent,
+  AboutMeComponent,
+	],
+	imports: [BrowserModule, NgbModule, MatButtonModule, AppRoutingModule, BrowserAnimationsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
