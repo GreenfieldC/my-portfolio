b1989e282e6461b2598e21f43ed04e5602501118
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-04-18 16:42:28 +0200

    subdomains implemented

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -14,7 +14,7 @@ export class MyWorkComponent implements OnInit {
 			category: 'angular',
 			description: 'A card game, that works on multiple devices.',
 			img: 'ring-of-fire',
-			site: 'https://ring-of-fire-f47e7.web.app/',
+			site: 'https://ring-of-fire.christian-greenfield.de',
 			github: 'https://github.com/GreenfieldC/ringoffire',
 		},
 		{
@@ -30,7 +30,7 @@ export class MyWorkComponent implements OnInit {
 			category: 'js',
 			description: 'A small jump and run game, made with JavaScript.',
 			img: 'el-pollo-loco',
-			site: 'https://www.jonathan-bergqvist.com/my-skills',
+			site: 'https://el-pollo-loco.christian-greenfield.de',
 			github: 'https://github.com/GreenfieldC/elPolloLoco',
 		},
 		{
@@ -38,7 +38,7 @@ export class MyWorkComponent implements OnInit {
 			category: 'js',
 			description: 'A Kanban Board, made with JavaScript.',
 			img: 'kanban-board',
-			site: 'https://www.jonathan-bergqvist.com/my-skills',
+			site: 'https://join.christian-greenfield.de',
 			github: 'https://github.com/GreenfieldC/kanban-board',
 		},
 	];
