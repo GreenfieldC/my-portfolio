3e28a79d06b53d62792176506298b88bcc627400
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-04-18 11:07:07 +0200

    aos aninmation implemented

--- src/app/main-site/main-site.component.ts
+++ src/app/main-site/main-site.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
-  selector: 'app-main-site',
-  templateUrl: './main-site.component.html',
-  styleUrls: ['./main-site.component.scss']
+	selector: 'app-main-site',
+	templateUrl: './main-site.component.html',
+	styleUrls: ['./main-site.component.scss'],
 })
-export class MainSiteComponent {
-
+export class MainSiteComponent implements OnInit {
+	ngOnInit(): void {
+		AOS.init();
+	}
 }
