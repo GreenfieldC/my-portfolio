2c4ef7fc7cbb07b602c5164822c826a910d1aa31
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-05-05 16:45:39 +0200

    property binding and some minor scss changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+	linkedInLink: string = 'http://www.linkedin.com/in/christian-greenfield';
+	xingLink: string = 'https://www.xing.com/profile/Christian_Greenfield';
+	gitHubLink: string = 'https://github.com/GreenfieldC';
 }
