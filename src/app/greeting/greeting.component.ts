725f425ad55da308c9da2bd12f16f980c25c8222
GreenfieldC <101995422+GreenfieldC@users.noreply.github.com>
2023-05-30 17:47:30 +0200

    reduce projects

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-greeting',
-  templateUrl: './greeting.component.html',
-  styleUrls: ['./greeting.component.scss']
+	selector: 'app-greeting',
+	templateUrl: './greeting.component.html',
+	styleUrls: ['./greeting.component.scss'],
 })
-export class GreetingComponent {
-
-}
+export class GreetingComponent {}
